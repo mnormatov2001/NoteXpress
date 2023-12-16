2ad68917c7e374b192081433197e9fe401817d78
Muzaffar Normatov <mnormatov2001@gmail.com>
2023-12-16 04:36:40 +0300

    configured next-auth authorization

---
+++ app/api/auth/federated-logout/route.ts
@@ -0,0 +1,30 @@
+import { getToken } from "next-auth/jwt";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+  try {
+    const token = await getToken({
+      req,
+      secret: process.env.NEXTAUTH_SECRET,
+    });
+
+    if (!token?.id_token)
+      throw new Error(
+        "Without an id_token the user won't be" +
+          "redirected back from the IdP after logout."
+      );
+
+    const url =
+      process.env.END_SESSION_ENDPOINT +
+      "?id_token_hint=" +
+      token.id_token +
+      "&post_logout_redirect_uri=" +
+      process.env.POST_LOGOUT_REDIRECT_URI;
+
+    return NextResponse.redirect(url);
+  } catch (error) {
+    console.error(error);
+  }
+
+  return NextResponse.redirect(process.env.NEXTAUTH_URL!);
+}
---
+++ configs/authOptions.ts
@@ -0,0 +1,32 @@
+import { AuthOptions } from "next-auth";
+import Auth0 from "next-auth/providers/auth0";
+
+export const authOptions: AuthOptions = {
+  providers: [
+    Auth0({
+      clientId: "notes-app-client",
+      clientSecret: "2RxPARYpxK3sVCws3CIS9vwgUpp3ImXWBjRT/OJJApc=",
+      authorization: {
+        params: {
+          client_id: "notes-app-client",
+          audience: "notes.app.webApi",
+          scope: "notes.app.webApi offline_access openid profile",
+          response_type: "code",
+        },
+      },
+      name: "my_auth0",
+      issuer: "https://localhost:7291",
+    }),
+  ],
+  debug: true,
+  callbacks: {
+    jwt: async ({ token, account }) => {
+      if (account) {
+        token.id_token = account.id_token;
+        token.refresh_token = account.refresh_token;
+      }
+
+      return token;
+    },
+  },
+};
