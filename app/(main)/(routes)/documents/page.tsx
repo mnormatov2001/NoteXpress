d2bc33c54f812e6dfefe8291c869055a541d72c1
Muzaffar Normatov <mnormatov2001@gmail.com>
2023-12-25 02:24:39 +0300

    added document empty page

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,9 +1,36 @@
-const DocumentsPage = () => {
-    return (
-        <div>
-            This is a protected page.
-        </div>
-    );
-};
+"use client";
 
+import Image from "next/image";
+import { PlusCircle } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+
+const DocumentsPage = () => {
+  return ( 
+    <div className="h-full flex flex-col items-center justify-center space-y-4">
+      <Image
+        src="/empty.png"
+        height="300"
+        width="300"
+        alt="Empty"
+        className="dark:hidden"
+      />
+      <Image
+        src="/empty-dark.png"
+        height="300"
+        width="300"
+        alt="Empty"
+        className="hidden dark:block"
+      />
+      <h2 className="text-lg font-medium">
+        Welcome to NoteXpress
+      </h2>
+      <Button onClick={() => {}}>
+        <PlusCircle className="h-4 w-4 mr-2" />
+        Create a note
+      </Button>
+    </div>
+   );
+};
+ 
 export default DocumentsPage;
